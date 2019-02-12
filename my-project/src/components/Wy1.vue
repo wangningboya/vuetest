f50c4c71d7f76c28b305fd67d309969ce9c3bf1a
wangningboya <843096638@qq.com>
2019-02-12 17:34:01 +0800

    模板玩玩

---
+++ my-project/src/components/Wy1.vue
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+<template>
+  <div id="vue_det">
+    <h1>site : {{site}}</h1>
+    <h1>url : {{url}}</h1>
+    <h1>{{details()}}</h1>
+</div>
+</template>
+
+<script>
+new Vue({
+  el: '#vue_det',
+  data: {
+    site: '菜鸟教程',
+    url: 'www.runoob.com',
+    alexa: '10000'
+  },
+  methods: {
+    details: function () {
+      return this.site + ' - 学的不仅是技术，更是梦想！'
+    }
+  }
+})
+</script>
